d850d4afa66e09ed6c9796eb9c03b7b51467c9e0
AnTsvihun <144221082+AnTsvihun@users.noreply.github.com>
2023-12-02 20:17:20 +0200

    Add files via upload

---
+++ Lab 1.1 Address.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Address
+{
+    private string index;
+    private string country;
+    private string city;
+    private string street;
+    private string house;
+    private string apartment;
+
+    public string Index
+    {
+        get { return index; }
+        set { index = value; }
+    }
+
+    public string Country
+    {
+        get { return country; }
+        set { country = value; }
+    }
+
+    public string City
+    {
+        get { return city; }
+        set { city = value; }
+    }
+
+    public string Street
+    {
+        get { return street; }
+        set { street = value; }
+    }
+
+    public string House
+    {
+        get { return house; }
+        set { house = value; }
+    }
+
+    public string Apartment
+    {
+        get { return apartment; }
+        set { apartment = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Address myAddress = new Address();
+        myAddress.Index = "12345";
+        myAddress.Country = "Ukraine";
+        myAddress.City = "Kyiv";
+        myAddress.Street = "Main Street";
+        myAddress.House = "10";
+        myAddress.Apartment = "5";
+
+        Console.WriteLine("Address Information:");
+        Console.WriteLine($"Index: {myAddress.Index}");
+        Console.WriteLine($"Country: {myAddress.Country}");
+        Console.WriteLine($"City: {myAddress.City}");
+        Console.WriteLine($"Street: {myAddress.Street}");
+        Console.WriteLine($"House: {myAddress.House}");
+        Console.WriteLine($"Apartment: {myAddress.Apartment}");
+    }
+}
